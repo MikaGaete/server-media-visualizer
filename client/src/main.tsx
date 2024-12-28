2145df20530ee9e2bc6058f398ea2a973c22d768
MikaGaete <mikael.gaete@ug.uchile.cl>
2024-12-28 11:29:20 -0300

    Initial client commit + project gitignore

---
+++ client/src/main.tsx
@@ -0,0 +1,5 @@
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import {Landing} from "./pages/Landing.tsx";
+
+createRoot(document.getElementById('root')!).render(<Landing/>);
---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,7 @@
+export const Landing = () => {
+    return (
+        <div className={'bg-[#f1f1f1] w-screen min-h-screen flex flex-col items-center'}>
+
+        </div>
+    );
+};
