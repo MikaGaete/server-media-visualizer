50b50674a66a76662245ba6793498973db04cce1
MikaGaete <mikael.gaete@ug.uchile.cl>
2024-12-28 11:43:37 -0300

    Feat(client/pages): Added 'NavBar' component to the 'Landing' page.

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,12 @@
+import {NavBar} from "../components/NavBar.tsx";
+
 export const Landing = () => {
     return (
         <div className={'bg-[#f1f1f1] w-screen min-h-screen flex flex-col items-center'}>
-
+            <NavBar/>
+            <div className={'w-full min-h-[90vh] flex flex-col items-center justify-center'}>
+                a
+            </div>
         </div>
     );
 };
