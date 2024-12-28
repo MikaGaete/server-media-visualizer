457f966f74dcb08be5834b41732ed2496adc3a62
MikaGaete <mikael.gaete@ug.uchile.cl>
2024-12-28 16:51:31 -0300

    Feat(client/router): Added route lock to the landing route.

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Landing} from "../pages/Landing.tsx";
 import {Login} from "../pages/Login.tsx";
+import {Lock} from "./Lock.tsx";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={'/'} element={<Landing/>} />
+                <Route path={'/'} element={<Lock><Landing/></Lock>} />
                 <Route path={'/login'} element={<Login/>} />
             </Routes>
         </BrowserRouter>
