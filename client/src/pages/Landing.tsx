a0954a5fd1b9ee213537523bbc01034d90786b87
MikaGaete <mikael.gaete@ug.uchile.cl>
2024-12-29 18:06:17 -0300

    Feat(client/pages): Added file upload fields.

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,11 +1,26 @@
 import {NavBar} from "../components/NavBar.tsx";
+import {FileInput} from "../templates/FileInput.tsx";
 
 export const Landing = () => {
+
+
     return (
-        <div className={'bg-[#f1f1f1] w-screen min-h-screen flex flex-col items-center'}>
+        <div className={'bg-[#f1f1f1] w-screen min-h-screen flex flex-col items-center scrollbar-hide'}>
             <NavBar/>
-            <div className={'w-full min-h-[90vh] flex flex-col items-center justify-center'}>
-                a
+            <div className={'w-full max-w-[100vw] h-[90vh] flex justify-center mt-[10vh] p-8'}>
+                <div className={'w-[50%] min-h-full flex flex-col gap-4 overflow-y-auto scrollbar-hide px-8'}>
+                    <h1 className={'text-6xl raleway-normal-600'}>Photos</h1>
+                    <div>
+                        <FileInput id={crypto.randomUUID()} accept={'image/*'} endpoint={'images'}/>
+                    </div>
+                </div>
+                <div className={'h-full w-0.5 bg-gray-400 rounded-lg'}></div>
+                <div className={'w-[50%] min-h-full flex flex-col gap-4 overflow-y-auto scrollbar-hide px-8'}>
+                    <h1 className={'text-6xl raleway-normal-600'}>Docs</h1>
+                    <div>
+                        <FileInput id={crypto.randomUUID()} accept={'.pdf,.txt,.docx,.doc,.rtf,.xlsx,.xls,.pptx,.ppt'} endpoint={'docs'}/>
+                    </div>
+                </div>
             </div>
         </div>
     );
