291006b273ddcd644c1aeb8a32c4f799ad7129f5
MikaGaete <mikael.gaete@ug.uchile.cl>
2024-12-29 21:39:34 -0300

    Chore(client/templates): Minor changes

--- client/src/templates/CustomButton.tsx
+++ client/src/templates/CustomButton.tsx
@@ -2,7 +2,7 @@ import {CustomButtonProps} from "../types/CustomButtonTypes.tsx";
 
 export const CustomButton = ({type, text, name, handleClick, hidden}: CustomButtonProps) => {
     return (
-        <button className={`${hidden && 'hidden'} bg-[#181818] text-white raleway-normal-400 text-sm py-2 rounded-lg`} type={type} name={name} onClick={handleClick}>
+        <button className={`${hidden && 'hidden'} bg-[#181818] text-white raleway-normal-400 text-medium py-2 px-10 rounded-lg`} type={type} name={name} onClick={handleClick}>
             {text}
         </button>
     );
